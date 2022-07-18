333f6680c937daff672dc9e494d27a36170a3e71
OlegFilippoff <5157175@list.ru>
2022-07-18 19:33:47 +0300

    reworked tests for gameStore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -41,10 +41,10 @@ public class GameStoreTest {
         Player player1 = new Player("Aaron");
         store.addPlayTime(player1.getName(), 5);
 
-        String actual = store.getMostPlayer();
-        String expected = "Aaron";
+        String[] actual = {store.getMostPlayer()};
+        String[] expected = {"Aaron"};
 
-        assertEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
@@ -69,28 +69,14 @@ public class GameStoreTest {
         store.addPlayTime(player1.getName(), 0);
         store.addPlayTime(player2.getName(), 1);
 
-        String actual = store.getMostPlayer();
-        String expected = "Bob";
+        String[] actual = {store.getMostPlayer()};
+        String[] expected = {"Bob"};
 
-        assertEquals(expected, actual);
+        assertArrayEquals(expected, actual);
 
         // если кол-во часов менее 1 часа(включительно), то работа метода ломается.
     }
 
-//    @Test
-//    void shouldAddPlayerTimeLessZero() {
-//        GameStore store = new GameStore();
-//        Player player1 = new Player("Aaron");
-//        Player player2 = new Player("Bob");
-//        store.addPlayTime(player1.getName(), -1);
-//        store.addPlayTime(player2.getName(), -9);
-//
-//        String actual = store.getMostPlayer();
-//        String expected = null;
-//
-//        assertEquals(expected, actual);
-//
-//    }
 
     @Test
     void shouldAddPlayerTimeToTime() {
@@ -120,8 +106,8 @@ public class GameStoreTest {
         store.addPlayTime(player2.getName(), 4);
         store.addPlayTime(player2.getName(), 0);
 
-        String actual = store.getMostPlayer();
-        String expected = "Bob";
+        String[] actual = {store.getMostPlayer()};
+        String[] expected = {"Bob"};
 
         assertEquals(expected, actual);
 
